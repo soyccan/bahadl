9c00f19c41a6d6c21cf5421025943ee9a33c2845
soytw <soytwww@gmail.com>
2018-02-03 11:52:20 +0800

    init

---
+++ contentscript.js
@@ -0,0 +1,71 @@
+$(function() {
+    $(document.body).append(`
+        <script>
+            var adSID = "9487";
+            $.ajax("/ajax/videoCastcishu.php?s=" + adSID + "&sn=" + animefun.videoSn);
+            $.ajax({
+                url: "/ajax/videoCastcishu.php?s=" + adSID + "&sn=" + animefun.videoSn + "&ad=end",
+                success: function() {
+                    $.ajax({
+                        url: "/ajax/m3u8.php?sn=" + animefun.videoSn + "&device=" + animefun.getdeviceid(),
+                        success: function(r) {
+                            var input = document.createElement('input');
+                            input.type = 'hidden';
+                            input.id = 'ip0873_';
+                            input.value = 'http:' + JSON.parse(r).src;
+                            document.body.appendChild(input);
+                        }
+                    });
+                }
+            });
+        </script>
+    `);
+    $('<iframe id="if9_3x4">').on('load', function() { // onload ????
+        console.log('send iframeLoadComplete '+this.src);
+        chrome.runtime.sendMessage({
+            info: 'iframeLoadComplete',
+            subPageURL: this.src });
+    }).appendTo(document.body).hide();
+});
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log('content script receive message');
+    console.log('href ' + location.href);
+    console.log(request);
+
+    if (request.action == 'getStreamURL') {
+        if (request.subPageURL == location.href) {
+            console.log('send response getStreamURL ' + $('#ip0873_').val());
+            sendResponse($('#ip0873_').val());
+        }
+        else {
+            console.log('getStreamURL you\'ve found the wrong person');
+        }
+    }
+    else if (request.action == 'getEpisodeURLs') {
+        if (request.mainPageURL == location.href) {
+            var urls = [];
+            $('#vul0_000 > li > a').each(function(idx, elem) {
+                urls.push(elem.href);
+            });
+            console.log('send response getEpisodeURLs: ' + urls);
+            sendResponse(urls);
+        }
+        else {
+            console.log('getEpisodeURLs none of my business');
+        }
+    }
+    else if (request.action == 'setIframeURL') {
+        if (request.mainPageURL == location.href) {
+            var fr = document.getElementById('if9_3x4');
+            console.log('setting iframe ' + request.subPageURL);
+            console.log(fr.src);
+            // if (fr.src !== request.subPageURL)
+                fr.src = request.subPageURL;
+        }
+        else {
+            console.log('setIframeURL i have no idea');
+        }
+    }
+});
---
+++ popup.js
@@ -0,0 +1,111 @@
+console.log('popup onClicked');
+
+var episodes = []; // id, pageURL, streamURL, tabId, frameId
+var loading;
+
+function getNotProcessedEpisode() {
+    var i = 0;
+    while (i < episodes.length && episodes[i].streamURL) i++;
+    if (i == episodes.length)
+        return null;
+    else
+        return episodes[i];
+}
+
+function setIframeURL(url, tab) {
+    console.log('send request setIframeURL ' +url);
+
+    chrome.tabs.sendMessage(
+        tab.id,
+        {action: 'setIframeURL', mainPageURL: tab.url, subPageURL: url},
+        function(_) {});
+}
+
+function getStreamURL(url, tab) {
+    console.log('send getStreamURL ' + url);
+
+    var ep = episodes.filter(e => e.pageURL == url)[0];
+    // if (!ep) getEpisodeURLs(tab);
+
+    chrome.tabs.sendMessage(
+      tab.id,
+      {action: 'getStreamURL', mainPageURL: tab.url, subPageURL: url},
+      function(streamURL) {
+        console.log('receive response getStreamURL: ' + streamURL);
+
+        if (streamURL) {
+            ep.streamURL = streamURL;
+            console.log('set episode');
+            console.log(ep);
+        }
+        else {
+            // TODO retry
+            console.log(chrome.runtime.lastError);
+        }
+
+        ep = getNotProcessedEpisode();
+        if (ep != null)
+            setIframeURL(ep.pageURL, tab);
+        else {
+            clearInterval(loading);
+            $('#output').val(episodes.map(e => e.streamURL).join('\n'));
+        }
+    });
+}
+
+function getEpisodeURLs(tab) {
+    chrome.tabs.sendMessage(
+      tab.id,
+      {action: 'getEpisodeURLs', mainPageURL: tab.url},
+      function(response) {
+        if (response && response.length > 0) {
+            for (var i in response) {
+                episodes.push({
+                    id: i,
+                    pageURL: response[i],
+                    streamURL: '',
+                });
+            }
+
+            console.log('getEpisodeURLs');
+            console.log(episodes);
+
+            setIframeURL(getNotProcessedEpisode().pageURL, tab);
+        }
+        else {
+            console.log(chrome.runtime.lastError);
+        }
+    });
+}
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var currentTab = tabs[0];
+
+    $(function() {
+        $('#cpbtn').click(function() {
+            $('#output').select();
+            if (!document.execCommand('copy'))
+                console.log('copy failed');
+        });
+
+        if (currentTab.url.startsWith('https://ani.gamer.com.tw/animeVideo.php?sn=')) {
+            loading = setInterval(function() {
+                var output = document.getElementById('output');
+                if (0 < output.value.length && output.value.length < 12) // Loading.....
+                    output.value += '.';
+                else
+                    output.value = 'Loading';
+            }, 500);
+        }
+    });
+
+    chrome.runtime.onMessage.addListener(
+      function(request, sender, sendResponse) {
+        if (request.info == 'iframeLoadComplete' && request.subPageURL) {
+            console.log('receive' +request.info+' '+request.subPageURL);
+            getStreamURL(request.subPageURL, currentTab);
+        }
+    });
+
+    getEpisodeURLs(currentTab);
+});
